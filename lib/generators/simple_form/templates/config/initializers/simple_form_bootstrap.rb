1a26192a7d28a1e59f3f7c56b643ae3ef04d93d2
Reuben Mallaby <reuben@mallaby.me>
2016-03-21 10:21:04 +0100

    Adds missing bootstrap columns

--- lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
+++ lib/generators/simple_form/templates/config/initializers/simple_form_bootstrap.rb
@@ -11,7 +11,7 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label, class: 'control-label'
+    b.use :label, class: 'col-sm-3 control-label'
 
     b.use :input, class: 'form-control'
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
@@ -23,7 +23,7 @@ SimpleForm.setup do |config|
     b.use :placeholder
     b.optional :maxlength
     b.optional :readonly
-    b.use :label, class: 'control-label'
+    b.use :label, class: 'col-sm-3 control-label'
 
     b.use :input
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
@@ -34,7 +34,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.optional :readonly
 
-    b.wrapper tag: 'div', class: 'checkbox' do |ba|
+    b.wrapper tag: 'div', class: 'checkbox col-sm-9' do |ba|
       ba.use :label_input
     end
 
@@ -45,7 +45,7 @@ SimpleForm.setup do |config|
   config.wrappers :vertical_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
-    b.use :label, class: 'control-label'
+    b.use :label, class: 'col-sm-3 control-label'
     b.use :input
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
@@ -115,7 +115,7 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label, class: 'sr-only'
+    b.use :label, class: 'col-sm-3 sr-only'
 
     b.use :input, class: 'form-control'
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
@@ -125,8 +125,8 @@ SimpleForm.setup do |config|
   config.wrappers :multi_select, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
-    b.use :label, class: 'control-label'
-    b.wrapper tag: 'div', class: 'form-inline' do |ba|
+    b.use :label, class: 'col-sm-3 control-label'
+    b.wrapper tag: 'div', class: 'col-sm-9 form-inline' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
